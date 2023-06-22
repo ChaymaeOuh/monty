d4330d8150078b3344b3789249ef96d926ab796a
ChaimaaFr <chaimaefarkouchi@gmail.com>
2023-06-22 10:49:18 -0700

    first commit

--- queue.c
+++
@@ -1,8 +0,0 @@
-#include "monty.h"
-
-void function_queue
-(stack_t **stack, unsigned int counter)
-{
-    (void)counter;
-    (void)stack;
-}
--- stack.c
+++
@@ -1,7 +0,0 @@
-#include "monty.h"
-
-void function_stack(stack_t **stack, unsigned int counter)
-{
-    (void)counter;
-    (void)stack;
-}
